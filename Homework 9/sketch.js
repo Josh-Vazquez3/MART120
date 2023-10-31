a21f39a8653de979211c0f25ae3ba8927625f331
Josh-Vazquez3 <123330441+Josh-Vazquez3@users.noreply.github.com>
2023-10-30 21:12:29 -0600

    Add files via upload

---
+++ Homework 9/sketch.js
@@ -0,0 +1,23 @@
+function setup()
+            {
+                createCanvas(500,600)
+            }
+
+            function draw()
+            {
+                background(120,45,78);
+                fill(255, 204, 0);
+                circle (250,100,175);
+                fill(25, 23, 130);
+                rect(200,190,100,150);
+              strokeWeight(10)
+              point(225,75);
+              point(300,75,75,);
+              triangle(30, 75, 58, 20, 86, 75);
+              line(5, 20, 85, 75);
+              point(30, 75);
+               fill(255, 204, 0);
+              text('Joshua Vazquez', 10, 375);
+              fill(2, 400, 0);
+              text('Title: Mini Me ', 400, 10);
+            }
