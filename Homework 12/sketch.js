c18315af00b3f88cf7dc8d0d98c9074a70722469
Josh-Vazquez3 <123330441+Josh-Vazquez3@users.noreply.github.com>
2023-11-20 19:49:32 -0700

    Add files via upload

---
+++ Homework 12/sketch.js
@@ -0,0 +1,158 @@
+// x and y for my character
+var characterX = 80;
+var characterY = 70;
+// define the key codes for each letter
+var w = 87; 
+var s = 83;
+var a = 65;
+var d = 68;
+
+// x and y for a shape
+var shapeX = 30;
+var shapeY = 50;
+var shapeXSpeed;
+var shapeYSpeed;
+
+// create a shape when the mouse is clicked
+var mouseShapeX;
+var mouseShapeY;
+function setup()
+{
+    createCanvas(500, 600);
+    // get a random speed when the it first starts
+    shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+    createCharacter(20,30);
+}
+function draw()
+{
+    background(10,45,88);
+    stroke(0);
+    fill(35);
+   // call createBoarders function
+    createBoarders();
+    // exit message
+    textSize(30);
+    text("GameOver", width-600,height-0)
+
+    //createCharacter(40,50);
+    drawCharacter();
+    characterMovement();
+  
+
+    // potential enemy
+    fill(200,00,7);
+    // draw the shape
+    square(shapeX, shapeY, 90);
+  // potential enemy
+    fill(200,00,7);
+    circle(shapeX, shapeY, 60);
+
+     // get a random speed when the it first starts
+     shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+     shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+
+    // move the shape
+    shapeX += shapeXSpeed;
+    shapeY += shapeYSpeed;
+    // check to see if the shape has gone out of bounds
+    if(shapeX > width)
+    {
+        shapeX = 0;
+    }
+    if(shapeX < 0)
+    {
+        shapeX = width;
+    }
+    if(shapeY > height)
+    {
+        shapeY = 0;
+    }
+    if(shapeY < 0)
+    {
+        shapeY = height;
+    }
+
+    // check to see if the character has left the exit
+    if(characterX > width && characterY > width-50)
+    {
+        fill(0);
+        stroke(5);
+        textSize(26);
+        text("GameOver", width/2-50, height/2-50);
+    }
+
+    // create the shape based on the mouse click
+    fill(120,130,140);
+    circle(mouseShapeX, mouseShapeY, 25);
+}
+
+function characterMovement()
+{
+    // handle the keys
+    if(keyIsDown(w))
+    {
+        characterY -= 10;   
+    }
+    if(keyIsDown(s))
+    {
+        characterY += 10;   
+    }
+    if(keyIsDown(a))
+    {
+        characterX -= 10;   
+        console.log("movement: " + characterX);
+    }
+    if(keyIsDown(d))
+    {
+        characterX += 10;   
+    }
+}
+function createCharacter(x,y)
+{
+    characterX = x;
+    characterY = y;
+    console.log(characterX);
+    //character
+    
+   // circle(characterX,characterY,25);
+}
+function drawCharacter()
+{
+    fill(23,40,123);
+    circle(characterX,characterY,25);
+}
+function createBorders(thickness)
+{
+    // top border
+    rect(0,0,width,thickness);
+    // left border
+    rect(0,0,thickness,height);
+    // bottom border
+    rect(0, height-thickness,width, thickness);
+    // right upper border
+    rect(width-thickness,0,thickness,height-50);
+}
+function createCharacter()
+{
+  //character
+  fill(23,40,123)
+  circle(characterX,characterY,25);
+}
+function createBoarders()
+{
+   // top border
+    rect(0,0,width,10);
+    // left border
+    rect(0,0,10,height);
+    // bottom border
+    rect(0, height-10,width,10);
+    // right upper border
+    rect(width-10,0,10,height-50);
+}
+function mouseClicked()
+{
+    mouseShapeX = mouseX;
+    mouseShapeY = mouseY;
+}
+           
